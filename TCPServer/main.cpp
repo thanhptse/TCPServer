b629b5d2f2e712e4ca9cfaea6bb2e87690fc67f4
Thanh Phan <thanhptse@gmail.com>
2020-10-06 01:12:18 +0700

    update more comments

--- TCPServer/main.cpp
+++ TCPServer/main.cpp
@@ -7,8 +7,8 @@
 // 1: Socket()
 // 2: Bind()
 // 3: Listen()
-// 4: Accept()
-// 5: Connect()
+// 4: Accept() - Called by server
+// 5: Connect() - Called by Client
 // 6: Read() / Send() / Sendto()
 // 7: Write() / Recv() / Recvfrom()
 // 8: Close()
@@ -23,25 +23,31 @@ void main() {
 		return;
 	}
 
-	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	// 1: Socket() - A connection Endpoint
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0); //SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+	//  function socket(...) return a socket description  on success and -1 on an error
+	// so that why have have this if
 	if (listening == INVALID_SOCKET) {
 		std::cerr << "Cant create a socket" << std::endl;
 		return;
 	}
 
 	sockaddr_in hint;
-	hint.sin_family = AF_INET;
+	hint.sin_family = AF_INET; // AF_INET and PF_INET are the same thing
 	hint.sin_port = htons(54000); // Host Byte Order to Network Byte Order (short)
-	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; // All IP address available for an interface
 
+	// 2: Bind() - Attaching to an IP and port
 	bind(listening, (sockaddr*)&hint, sizeof(hint));
 
-	listen(listening, SOMAXCONN);
+	// 3:Listen() - Wait for a connection
+	listen(listening, SOMAXCONN); // We can replace SOMAXCONN to other number like 10, so that we will allow a maximum of 10 connections to be in pending state
 
 	sockaddr_in client;
 	int clientSize = sizeof(client);
 
-	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	// 4: Accept() - A new connection
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize); // accept new connections from new clients try to connect to the server
 
 	char host[NI_MAXHOST]; // Client's name
 	char service[NI_MAXSERV]; // Port the client is connect on
@@ -61,6 +67,7 @@ void main() {
 
 	char buf[4096];
 
+	// 6,7: Send / Recv - Data(receive data from `client` and send back something to their)
 	while (true) {
 		ZeroMemory(buf, 4096);
 
@@ -76,6 +83,7 @@ void main() {
 		send(clientSocket, buf, bytesReceived + 1, 0);
 	}
 
+	// 8: Close() - Bye byeeee
 	closesocket(clientSocket);
 
 	WSACleanup();
